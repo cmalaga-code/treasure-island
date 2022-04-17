08c7c43c61d4c6292ebf948e65f1fac74fedb4d1
Cristian Malaga <cmalaga0605@gmail.com>
2022-04-16 21:09:16 -0400

    update python

--- components/main.py
+++ components/main.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
     print("Welcome to treasure island!!")
     player_status = logic.first_trial()
     if player_status:
-        print("Great Job! Next question..")
+        print("Great job! Next question..")
         player_status = logic.second_trial()
         if player_status:
-            print("Great Job! You made the correct choice..")
+            print("Great job! You made the correct choice..")
             player_status = logic.thrid_trial()
             if player_status:
                 print("You win !! Congrats !")
             else:
-                print("Game Over!! Try again.")
+                print("Game over!! Try again.")
         else:
-            print("Game Over!! Try again.")
+            print("Game over!! Try again.")
     else:
-        print("Game Over!! Try again.")
+        print("Game over!! Try again.")
